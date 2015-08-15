1e207655bfe4ab10b3e630ba6c98e69d10eba4da
Rob Menke <rmenke@gmail.com>
2015-08-15 15:12:57 -0700

    Initial Commit

---
+++ Hough/HoughPlugIn.h
@@ -0,0 +1,17 @@
+//
+//  HoughPlugIn.h
+//  Hough
+//
+//  Created by Rob Menke on 8/15/15.
+//  Copyright (c) 2015 Rob Menke. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface HoughPlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
